2c276d9f81930eb257f443777aa724cea317dfa4
“petre007” <petredumitru1@gmail.com>
2024-04-02 18:28:31 +0300

    Init flow

---
+++ flow/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/create_connexion', methods=['POST'])
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
