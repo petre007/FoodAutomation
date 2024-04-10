cca55c4a8b68a6520430a984fd0392871e9fa75f
“petre007” <petredumitru1@gmail.com>
2024-04-10 20:16:04 +0300

    Improve RobotEnv and model trainning + prepare for trainning robot in reallife

--- flow/brain/environment.py
+++ flow/brain/environment.py
@@ -1,10 +1,21 @@
 import gym
 import matplotlib
+import cv2
+import base64
+import math
+from pupil_apriltags import Detector
+
+from collector.data_collector import DataCollector
 
 matplotlib.use('TkAgg')
 from gym import spaces
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+def decode_image_from_esp32(image):
+    esp32_image = np.frombuffer(base64.b64decode(image), np.uint8)
+    return cv2.imdecode(esp32_image, cv2.IMREAD_GRAYSCALE)
 
 
 class CustomLayout(object):
@@ -15,9 +26,12 @@ class CustomLayout(object):
         self.layout = np.zeros((self.length, self.height))
         self.obstacles = []
         self.robot_position = []
+        # make targets an object that holds the relative positions of AprilTags and their actual image
         self.targets = []
+        self.apriltag_detected = False
+        self.obstacle_detected = False
 
-    def create_obstacles(self, start_coord, end_coord):
+    def _create_obstacles(self, start_coord, end_coord):
         start_row, start_col = start_coord
         end_row, end_col = end_coord
 
@@ -30,34 +44,44 @@ class CustomLayout(object):
             for col in range(start_col, end_col + 1):
                 self.obstacles.append((row, col))
 
+    def generate_obstacles(self):
+        self._create_obstacles([50, 0], [50, 999])
+        self._create_obstacles([300, 0], [300, 40])
+        self._create_obstacles([300, 40], [700, 40])
+        self._create_obstacles([700, 0], [700, 40])
+        self._create_obstacles([450, 500], [450, 700])
+        self._create_obstacles([450, 500], [750, 500])
+        self._create_obstacles([750, 500], [750, 700])
+        self._create_obstacles([450, 700], [750, 700])
+
 
 class RobotEnv(gym.Env):
     def __init__(self):
         super(RobotEnv, self).__init__()
-
+        self.data_collector = DataCollector()
         # Define action and observation space
         self.action_space = spaces.Discrete(4)  # Four discrete actions: forward, backward, left, right
-        self.observation_space = spaces.Box(low=0, high=100, shape=(5,),
+        self.observation_space = spaces.Box(low=0, high=100, shape=(1000,),
                                             dtype=np.float32)  # Four ultrasonic sensor readings + AprilTag detection
         self.custom_layout = CustomLayout(1000, 1000)
         # Initialize robot's position and sensors
         self.custom_layout.robot_position = [100, 800]  # Robot's initial position (x, y)
-        # Example obstacle positions (x, y)
+        # Target positions (x, y)
         self.custom_layout.targets = [600, 300]  # AprilTag position (x, y)
-
-        self.custom_layout.create_obstacles([50, 0], [50, 999])
-        self.custom_layout.create_obstacles([300, 0], [300, 40])
-        self.custom_layout.create_obstacles([300, 40], [700, 40])
-        self.custom_layout.create_obstacles([700, 0], [700, 40])
-        self.custom_layout.create_obstacles([450, 500], [450, 700])
-        self.custom_layout.create_obstacles([450, 500], [750, 500])
-        self.custom_layout.create_obstacles([750, 500], [750, 700])
-        self.custom_layout.create_obstacles([450, 700], [750, 700])
+        # Obstacles positions
+        self.custom_layout.generate_obstacles()
+        # Detection of apriltag
 
     def reset(self, **kwargs):
         # Reset robot's position and return initial sensor readings
         self.custom_layout.robot_position = [100, 800]
         return self._get_observation()
+
+    def _get_min_distance_from_layout_obstacles(self):
+        distances = []
+        for obs_pos in self.custom_layout.obstacles:
+            distances.append(math.dist(obs_pos, self.custom_layout.robot_position))
+        return distances
 
     def step(self, action):
         # Update robot's position based on action
@@ -71,40 +95,51 @@ class RobotEnv(gym.Env):
             self.custom_layout.robot_position[0] += 1
 
         # Clip robot's position to stay within the environment bounds
-        self.custom_layout.robot_position = np.clip(self.custom_layout.robot_position, self.custom_layout.height, self.custom_layout.length)
+        self.custom_layout.robot_position = np.clip(self.custom_layout.robot_position,
+                                                    self.custom_layout.height,
+                                                    self.custom_layout.length)
 
-        # Check if robot has collided with an obstacle
+        # Check if robot has collided with an obstacle or with the layout
         reward = 0
-        for obstacle_pos in self.custom_layout.obstacles:
-            if np.array_equal(self.custom_layout.robot_position, obstacle_pos):
-                reward = -1  # Negative reward for collision
-                break
+        if self.custom_layout.obstacle_detected or min(self._get_min_distance_from_layout_obstacles()) < 15:
+            reward -= -1
 
         # Check if robot has reached AprilTag
         if np.array_equal(self.custom_layout.robot_position, self.custom_layout.targets):
-            reward = 10  # Positive reward for reaching AprilTag
+            reward += 10  # Positive reward for reaching AprilTag
+
+        # Check if robot detected AprilTag
+        if self.custom_layout.apriltag_detected:
+            reward += 5
 
         # Return new observation, reward, and done flag
         observation = self._get_observation()
-        done = np.array_equal(self.custom_layout.robot_position,
-                              self.custom_layout.targets)  # Episode ends when robot reaches AprilTag
-        return observation, reward, done, {}
+        return observation, reward
 
     def _get_observation(self):
-        # Simulate ultrasonic sensor readings based on robot's position and obstacle positions
-        sensor_readings = []
-        for obstacle_pos in self.custom_layout.obstacles:
-            distance = np.sqrt(
-                (self.custom_layout.robot_position[0] - obstacle_pos[0]) ** 2 + (
-                            self.custom_layout.robot_position[1] - obstacle_pos[1]) ** 2)
-            sensor_readings.append(distance)
+        # Use actual data from ultrasonic to avoid dynamic obstacles
 
-        # Simulate AprilTag detection
-        apriltag_detected = int(np.array_equal(self.custom_layout.robot_position, self.custom_layout.targets))
+        sensor_readings = self.data_collector.ultrasonic_data
+        if min(sensor_readings) <= 15:
+            self.custom_layout.obstacle_detected = True
+        else:
+            self.custom_layout.obstacle_detected = False
 
-        return np.array(sensor_readings + [apriltag_detected])
+        # Use the actual robot to retrieve and compare if he got to see an actual apriltag
+        image_readings = self.data_collector.esp32_data
+        for image in image_readings:
+            image_decoded = decode_image_from_esp32(image)
+            if len(Detector().detect(image_decoded)) != 0:
+                self.custom_layout.apriltag_detected = True
+                break
+            else:
+                self.custom_layout.apriltag_detected = False
 
-    def render(self, mode='human'):
+        reached_target = int(np.array_equal(self.custom_layout.robot_position, self.custom_layout.targets))
+
+        return reached_target, self.custom_layout.obstacle_detected, self.custom_layout.apriltag_detected
+
+    def render(self):
         plt.figure()
         plt.imshow(self.custom_layout.layout)  # Initialize empty plot
 
@@ -117,6 +152,6 @@ class RobotEnv(gym.Env):
 
         # Plot target (green)
         plt.scatter(self.custom_layout.targets[0], self.custom_layout.targets[1], color='green')
-
+        plt.legend(['Obstacles', 'Robot', 'Target'])
         plt.title('Robot Environment')
         plt.show()
--- flow/brain/rl_model.py
+++ flow/brain/rl_model.py
@@ -1,24 +1,13 @@
 import os
 import random
-from keras.src.layers import Dense, Conv2D, Flatten
+from keras.src.layers import Dense, Conv2D, Flatten, concatenate
 from collections import deque
 from brain.environment import RobotEnv
-from collector.data_collector import DataCollector
 
 os.environ["KERAS_BACKEND"] = "tensorflow"
 
-import cv2
-from keras import Sequential
+from keras import Sequential, Model
 import numpy as np
-
-
-def decode_image_from_esp32():
-    data_collector = DataCollector()
-    esp32_image_base64 = data_collector.esp32_data
-    esp32_images = []
-    for image in esp32_image_base64:
-        esp32_images = np.fromstring(image.decode('base64'), np.uint8)
-    return cv2.imdecode(esp32_images, cv2.IMREAD_ANYCOLOR)
 
 
 def create_obstacle_avoidance_model(input_shape):
@@ -31,8 +20,39 @@ def create_obstacle_avoidance_model(input_shape):
     return model
 
 
-def train_obstacle_avoidance_model(model):
-    env = RobotEnv()  # Create an instance of the environment
+def create_image_model(input_shape):
+    model = Sequential([
+        Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
+        Conv2D(64, (3, 3), activation='relu'),
+        Flatten(),
+        Dense(128, activation='relu'),
+        Dense(4, activation='softmax')
+    ])
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+
+def combine_models(obstacle_avoidance_model, image_model):
+    # Create obstacle avoidance model for sensor data
+    sensor_model = create_obstacle_avoidance_model(obstacle_avoidance_model)
+
+    # Create image model for image data
+    image_model = create_image_model(image_model)
+
+    # Combine both models
+    combined_input = concatenate([sensor_model.output, image_model.output])
+    combined_output = Dense(4, activation='softmax')(combined_input)  # Output layer for Left, Right, Forward, Back
+    combined_model = Model(inputs=[sensor_model.input, image_model.input], outputs=combined_output)
+
+    # Compile the combined model
+    combined_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return combined_model
+
+
+def train_model(model):
+    # Create an instance of the environment
+    env = RobotEnv()
 
     # Define training parameters
     num_episodes = 1000
@@ -48,27 +68,31 @@ def train_obstacle_avoidance_model(model):
         obs = np.reshape(obs, [1, -1])
         total_reward = 0
 
+        actions_taken = []  # List to store actions taken in each episode
+
         for step in range(max_steps_per_episode):
             # Choose action using epsilon-greedy policy
             if np.random.rand() <= epsilon:
                 action = env.action_space.sample()  # Explore
             else:
-                # Predict the probability of needing to "Stop"
-                probability_stop = model.predict(obs)[0][0]
-                # Convert probability to action (0 for "Go", 1 for "Stop")
-                action = 1 if probability_stop > 0.5 else 0
+                # Predict the probabilities of each action
+                action_probabilities = model.predict(obs)
+                # Choose action with highest probability
+                action = np.argmax(action_probabilities)
 
             # Take action in the environment
-            next_obs, reward, done, _ = env.step(action)
+            next_obs, reward = env.step(action)
             next_obs = np.reshape(next_obs, [1, -1])
 
             # Store transition in replay memory
-            memory.append((obs, action, reward, next_obs, done))
+            memory.append((obs, action, reward, next_obs))
 
             total_reward += reward
             obs = next_obs
 
-            if done:
+            actions_taken.append(action)  # Store the action taken
+
+            if next_obs[1]:
                 break
 
         # Experience replay
@@ -77,77 +101,6 @@ def train_obstacle_avoidance_model(model):
             obs_batch, action_batch, reward_batch, next_obs_batch, done_batch = zip(*minibatch)
             obs_batch = np.concatenate(obs_batch)
             next_obs_batch = np.concatenate(next_obs_batch)
-
-            target_batch = np.array(action_batch)  # Target is the action taken (0 or 1)
-            model.fit(obs_batch, target_batch, epochs=1, verbose=0)
-
-        # Decay epsilon
-        if epsilon > epsilon_min:
-            epsilon *= epsilon_decay
-
-        # Print episode info
-        print(f"Episode: {episode + 1}/{num_episodes}, Total Reward: {total_reward}, Epsilon: {epsilon}")
-
-    # Close the environment
-    env.close()
-
-
-def create_image_model(input_shape):
-    model = Sequential([
-        Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
-        Conv2D(64, (3, 3), activation='relu'),
-        Flatten(),
-        Dense(128, activation='relu'),
-        Dense(4, activation='softmax')  # Output layer with 4 units for LEFT, RIGHT, FORWARD, BACK
-    ])
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    return model
-
-
-def train_image_model(model):
-    env = RobotEnv()  # Create an instance of the environment
-
-    # Define training parameters
-    num_episodes = 1000
-    max_steps_per_episode = 100
-    epsilon = 1.0  # Exploration rate
-    epsilon_min = 0.01
-    epsilon_decay = 0.995
-    batch_size = 32
-    memory = deque(maxlen=2000)  # Replay memory
-
-    for episode in range(num_episodes):
-        obs = env.reset()  # Reset the environment at the beginning of each episode
-        total_reward = 0
-
-        for step in range(max_steps_per_episode):
-            # Choose action using epsilon-greedy policy
-            if np.random.rand() <= epsilon:
-                action = env.action_space.sample()  # Explore
-            else:
-                # Predict the probabilities of each action
-                action_probabilities = model.predict(obs[np.newaxis, :])
-                # Choose action with highest probability
-                action = np.argmax(action_probabilities)
-
-            # Take action in the environment
-            next_obs, reward, done, _ = env.step(action)
-
-            # Store transition in replay memory
-            memory.append((obs, action, reward, next_obs, done))
-
-            total_reward += reward
-            obs = next_obs
-
-            if done:
-                break
-
-        # Experience replay
-        if len(memory) >= batch_size:
-            minibatch = random.sample(memory, batch_size)  # Sample minibatch from replay memory
-            obs_batch, action_batch, reward_batch, next_obs_batch, done_batch = zip(*minibatch)
-            obs_batch = np.stack(obs_batch)
-            next_obs_batch = np.stack(next_obs_batch)
 
             target_batch = np.array(action_batch)  # Target is the action taken
             model.fit(obs_batch, target_batch, epochs=1, verbose=0)
