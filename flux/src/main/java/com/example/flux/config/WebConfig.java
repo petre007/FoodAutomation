9632a21373ee5d85281e4bd0c6c83474299117f2
“petre007” <petredumitru1@gmail.com>
2024-04-14 04:28:30 +0300

    Added CORS support for flutterflow web app

---
+++ flux/src/main/java/com/example/flux/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.flux.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://food-automation-u-i-9gobko.flutterflow.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
